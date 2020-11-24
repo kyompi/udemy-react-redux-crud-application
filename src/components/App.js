c4436dc8e018657ad0d2164fbfc59264d2261615
KYOMPI\kyompi <omaegawarui@gmail.com>
2020-11-24 10:38:44 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div className="App">
-        Hello, world!
-    </div>
-  );
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+import {increment, decrement} from '../actions'
+
+class App extends Component {
+    render() { 
+        const props = this.props
+
+        return (
+          <>
+            <div>value: {props.value}</div>
+            <button onClick={props.increment}>+1</button>
+            <button onClick={props.decrement}>-1</button>
+          </>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => ({ value: state.count.value })
+const mapDispatchToProps = ({increment, decrement})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
