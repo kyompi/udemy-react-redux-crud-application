7072efd0e10645d96cecc5de4c36728220198af0
KYOMPI\kyompi <omaegawarui@gmail.com>
2020-11-30 10:45:28 +0900

    implement event new component

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -4,7 +4,6 @@ import { READ_EVENTS } from '../actions'
 export default (events = {}, action) => {
     switch (action.type) {
         case READ_EVENTS:
-            console.log(action.response.data)
             return _.mapKeys(action.response.data, 'id')
         default:
             return events
